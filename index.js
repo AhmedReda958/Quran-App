c0fb667cf36733f8b1db47ebb7ff675e2b8a7842
Gad..! <59254228+AhmedReda958@users.noreply.github.com>
2022-02-08 19:53:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dailyPage from './components/tweetPage.js'
 
 // just tweet when the app run
 tweetRandomVerse(validator)
-
+/*
 setInterval(() => {
     let hour = new Date().getHours() 
     // 2 pm 
@@ -11,7 +11,7 @@ setInterval(() => {
         dailyPage()
 
     }
-}, 30*60*1000);
+}, 30*60*1000);*/
 
 // timer to tweet 
 // timer is set to 15m
